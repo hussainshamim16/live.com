c93d25ad04d3b75593f36be9b966148b8e64cec7
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-05-02 13:18:04 +0500

    live.com

---
+++ js Flow/app.js
@@ -0,0 +1,88 @@
+
+
+var din = document.getElementById("day");
+var raat = document.getElementById("night");
+
+
+var daay = document.getElementById("din");
+var niight = document.getElementById("raat");
+
+var logo = document.getElementById("logo")
+var style = document.getElementById("styleLink")
+
+
+function night(){
+    
+  style.href = "/styling/night.css"
+    logo.src = "/images/logo 1 (2).png";
+    raat.style.display = "none"
+    din.style.display = "flex"
+}
+function day(){
+    document.body.style.backgroundColor = ""
+    document.body.style.color = ""
+
+    logo.src = "/images/logo 1 (1).png";
+    style.href = ""
+    din.style.display = "none"
+    raat.style.display = "flex"
+}
+
+// second nav
+
+function raaat(){
+    
+    logo.src = "/images/logo 1 (2).png";
+    style.href = "/styling/night.css"
+    niight.style.display = "none"
+    daay.style.display = "flex"
+}
+function diin(){
+    document.body.style.backgroundColor = ""
+    document.body.style.color = ""
+
+    logo.src = "/images/logo 1 (1).png";
+    style.href = ""
+    daay.style.display = "none"
+    niight.style.display = "flex"
+}
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+  var navBar = document.getElementById("navBar")
+  var secnavBar = document.getElementById("secondNav")
+  var sec0 = document.getElementById("zero")
+  
+ 
+
+
+  
+  function remove(){
+sec0.style.display= "none";
+  }
+  function NavBar(){
+    secondNav.style.display= "block"; 
+  }
+  function Bar(){
+    secondNav.style.display= "none"; 
+  }
+
+  function NumberSir(){
+alert("031728xx")
+  }
+
+  document.getElementById("contactForm").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevent default form submission
+    
+    var email = document.getElementById("email").value;    
+    
+    // Open Gmail compose window with pre-filled email address and message
+    window.location.href = "mailto:hj422748@gmail.com?subject=New%20Message&body=Email%20Address:%20" + email;
+  });
+
